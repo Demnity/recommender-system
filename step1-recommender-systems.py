8bc6040d4f41198d2549d2e47893cf74a4e0d650
Dean Nguyen <d.v.q.nguyen@student.tudelft.nl>
2021-01-09 15:09:15 +0100

    Create data and user-user matrix functions
    Actual files too big to upload to Github

--- step1-recommender-systems.py
+++ step1-recommender-systems.py
@@ -1,9 +1,13 @@
+import sys
+
 import numpy as np
 import pandas as pd
 import os.path
 from random import randint
 
 # -*- coding: utf-8 -*-
+from numpy import savetxt
+
 """
 ### NOTES
 This file is an example of what your code should look like. It is written in Python 3.6.
@@ -16,25 +20,66 @@ To know more about the expectations, please refer to the guidelines.
 ##
 #####
 
-#Where data is located
+# Where data is located
 movies_file = './data/movies.csv'
 users_file = './data/users.csv'
 ratings_file = './data/ratings.csv'
 predictions_file = './data/predictions.csv'
 submission_file = './data/submission.csv'
 
-
 # Read the data using pandas
-movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID':'int', 'year':'int', 'movie':'str'}, names=['movieID', 'year', 'movie'])
-users_description = pd.read_csv(users_file, delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession'])
-ratings_description = pd.read_csv(ratings_file, delimiter=';', dtype={'userID':'int', 'movieID':'int', 'rating':'int'}, names=['userID', 'movieID', 'rating'])
+movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID': 'int', 'year': 'int', 'movie': 'str'},
+                                 names=['movieID', 'year', 'movie'])
+users_description = pd.read_csv(users_file, delimiter=';',
+                                dtype={'userID': 'int', 'gender': 'str', 'age': 'int', 'profession': 'int'},
+                                names=['userID', 'gender', 'age', 'profession'])
+ratings_description = pd.read_csv(ratings_file, delimiter=';',
+                                  dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
+                                  names=['userID', 'movieID', 'rating'])
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
+
+
+def create_data_matrix(ratings):
+    data_matrix = np.zeros((6040, 3706))
+    for i, row in ratings.iterrows():
+        data_matrix[row['userID'] - 1, row['movieID'] - 1] = row['rating']
+    np.save('data_matrix.npy', data_matrix)
+
+
+saved_data_matrix = np.load('data_matrix.npy')
+
 
 #####
 ##
 ## COLLABORATIVE FILTERING
 ##
 #####
+
+
+def cosine_similarity(vector_a, vector_b):
+    return np.dot(vector_a, vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))
+
+
+def create_user_user_matrix(m):
+    user_user_matrix = np.zeros((m.shape[0], m.shape[0]))
+
+    for i in range(m.shape[0]):
+        for j in range(i, m.shape[0]):
+            if i != j:
+                sim = cosine_similarity(m[i], m[j])
+                user_user_matrix[i, j] = sim
+                user_user_matrix[j, i] = sim
+
+            if i == j:
+                user_user_matrix[i, j] = 1
+
+    np.save('user_user_matrix_1.npy', user_user_matrix)
+
+
+user_user_matrix_1 = np.load('user_user_matrix_1.npy')
+
+print(user_user_matrix_1)
+
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
     # TO COMPLETE
@@ -47,13 +92,13 @@ def predict_collaborative_filtering(movies, users, ratings, predictions):
 ## LATENT FACTORS
 ##
 #####
-    
+
 def predict_latent_factors(movies, users, ratings, predictions):
     ## TO COMPLETE
 
     pass
-    
-    
+
+
 #####
 ##
 ## FINAL PREDICTORS
@@ -61,9 +106,9 @@ def predict_latent_factors(movies, users, ratings, predictions):
 #####
 
 def predict_final(movies, users, ratings, predictions):
-  ## TO COMPLETE
+    ## TO COMPLETE
 
-  pass
+    pass
 
 
 #####
@@ -72,12 +117,13 @@ def predict_final(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE
 ##
 #####
-    
-#By default, predicted rate is a random classifier
+
+# By default, predicted rate is a random classifier
 def predict_random(movies, users, ratings, predictions):
     number_predictions = len(predictions)
 
     return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
+
 
 #####
 ##
@@ -88,12 +134,12 @@ def predict_random(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE by your prediction function
 predictions = predict_random(movies_description, users_description, ratings_description, predictions_description)
 
-#Save predictions, should be in the form 'list of tuples' or 'list of lists'
+# Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
-    #Formates data
+    # Formates data
     predictions = [map(str, row) for row in predictions]
     predictions = [','.join(row) for row in predictions]
-    predictions = 'Id,Rating\n'+'\n'.join(predictions)
-    
-    #Writes it dowmn
+    predictions = 'Id,Rating\n' + '\n'.join(predictions)
+
+    # Writes it dowmn
     submission_writer.write(predictions)
