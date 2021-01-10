8e4d6dc49946000ff2d002983c955adb651bd0f7
Demnity <T.M.Dao@student.tudelft.nl>
2021-01-10 15:31:26 +0700

    latent factor + utility matrix

--- step1-recommender-systems.py
+++ step1-recommender-systems.py
@@ -37,16 +37,30 @@ ratings_description = pd.read_csv(ratings_file, delimiter=';',
                                   dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
                                   names=['userID', 'movieID', 'rating'])
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
+print(users_description.shape)
+print(movies_description.shape)
 
+# Creating and saving the utility matrix as csv
+# df = pd.DataFrame(0, index=np.arange(1, users_description.shape[0] + 1),
+#                   columns=np.arange(1, movies_description.shape[0] + 1))
+# for i, row in ratings_description.iterrows():
+#     df.loc[row['userID'], row['movieID']] = row['rating']
+# df.to_csv('./data/utility_matrix.csv')
 
-def create_data_matrix(ratings):
-    data_matrix = np.zeros((6040, 3706))
-    for i, row in ratings.iterrows():
-        data_matrix[row['userID'] - 1, row['movieID'] - 1] = row['rating']
-    np.save('data_matrix.npy', data_matrix)
+util_matrix = pd.read_csv('./data/utility_matrix.csv', index_col=0)
+util_matrix.columns = util_matrix.columns.astype(int)
+print(util_matrix.shape)
+print(util_matrix.head())
+print(util_matrix.loc[1, 1])
 
-
-saved_data_matrix = np.load('data_matrix.npy')
+# def create_data_matrix(ratings):
+#     data_matrix = np.zeros((6040, 3706))
+#     for i, row in ratings.iterrows():
+#         data_matrix[row['userID'] - 1, row['movieID'] - 1] = row['rating']
+#     np.save('data_matrix.npy', data_matrix)
+#
+#
+# saved_data_matrix = np.load('data_matrix.npy')
 
 
 #####
@@ -56,29 +70,29 @@ saved_data_matrix = np.load('data_matrix.npy')
 #####
 
 
-def cosine_similarity(vector_a, vector_b):
-    return np.dot(vector_a, vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))
-
-
-def create_user_user_matrix(m):
-    user_user_matrix = np.zeros((m.shape[0], m.shape[0]))
-
-    for i in range(m.shape[0]):
-        for j in range(i, m.shape[0]):
-            if i != j:
-                sim = cosine_similarity(m[i], m[j])
-                user_user_matrix[i, j] = sim
-                user_user_matrix[j, i] = sim
-
-            if i == j:
-                user_user_matrix[i, j] = 1
-
-    np.save('user_user_matrix_1.npy', user_user_matrix)
-
-
-user_user_matrix_1 = np.load('user_user_matrix_1.npy')
-
-print(user_user_matrix_1)
+# def cosine_similarity(vector_a, vector_b):
+#     return np.dot(vector_a, vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))
+#
+#
+# def create_user_user_matrix(m):
+#     user_user_matrix = np.zeros((m.shape[0], m.shape[0]))
+#
+#     for i in range(m.shape[0]):
+#         for j in range(i, m.shape[0]):
+#             if i != j:
+#                 sim = cosine_similarity(m[i], m[j])
+#                 user_user_matrix[i, j] = sim
+#                 user_user_matrix[j, i] = sim
+#
+#             if i == j:
+#                 user_user_matrix[i, j] = 1
+#
+#     np.save('user_user_matrix_1.npy', user_user_matrix)
+#
+#
+# user_user_matrix_1 = np.load('user_user_matrix_1.npy')
+#
+# print(user_user_matrix_1)
 
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
@@ -95,9 +109,18 @@ def predict_collaborative_filtering(movies, users, ratings, predictions):
 
 def predict_latent_factors(movies, users, ratings, predictions):
     ## TO COMPLETE
+    k = 20
+    u, s, vt = np.linalg.svd(util_matrix)
+    s = np.diag(s)
+    s = s[0:k, 0:k]
+    u = u[:, 0:k]
+    vt = vt[0:k, :]
+    print(u.shape, s.shape, vt.shape)
+    predict_all = np.dot(np.dot(u, s), vt)
+    return predict_all
 
-    pass
-
+util_matrix_predict = predict_latent_factors(movies_description, users_description, ratings_description, predictions_description)
+print(util_matrix_predict[0:5])
 
 #####
 ##
